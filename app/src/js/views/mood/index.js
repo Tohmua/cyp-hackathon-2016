4df675c3e91ec122cf18ca488f86ee8227ed45c5
Tohmua <oliver.musgrove-wethey@mayden.co.uk>
2016-04-23 23:14:30 +0100

    Swipe working example

--- app/src/js/views/mood/index.js
+++ app/src/js/views/mood/index.js
@@ -2,12 +2,10 @@ var Container = require('react-container');
 var Sentry = require('react-sentry');
 var React = require('react');
 var { animation, Link, Transitions } = require('../../touchstone');
-var Swiper = require('react-swiper');
+var Swipeable = require('react-swipeable')
 
 var EventEmitter = require('events').EventEmitter;
 var emitter = new EventEmitter();
-
-React.initializeTouchEvents(true);
 
 module.exports = React.createClass({
     displayName: 'ViewMe',
@@ -17,7 +15,7 @@ module.exports = React.createClass({
     statics: {
         navigationBar: 'main',
         getNavigation (props) {
-            var name = props.me && props.me.name
+            // var name = props.me && props.me.name
 
             return {
                 leftIcon: 'ion-android-menu',
@@ -30,56 +28,37 @@ module.exports = React.createClass({
         }
     },
 
-    getDefaultProps () {
-        return {
-            me: {}
-        }
-    },
-
-    componentDidMount () {
-        var self = this;
-        var body = document.getElementsByTagName('body')[0];
-        var menuWrapper = document.getElementsByClassName('Tabs-Navigator-wrapper')[0];
-        body.classList.remove('android-menu-is-open');
-        menuWrapper.addEventListener('click', function (e) {
-            body.classList.remove('android-menu-is-open');
-        });
-
-        // navbar actions
-        this.watch(emitter, 'navigationBarLeftAction', function () {
-            body.classList.toggle('android-menu-is-open');
-        });
-        this.watch(emitter, 'navigationBarRightAction', function () {
-            self.transitionTo('tabs:me-edit', { transition: 'fade' })
-        });
-    },
-
-    // render () {
-    //     const myElement = <span />
-    //     return (
-    //       <Swiper href="http://example.com" onSwipe={this.handleSwipe}>
-    //         Swipe or click me...
-    //       </Swiper>
-    //     );
-    // },
-
-    //   handleSwipe: function (e) {
-    //     console.log(e);
-    //   }
-
-
     render () {
         var content =
             <Container direction="column">
                 <Container fill align="center" justify="center" direction="column" scrollable className="MeRegistration__body">
-                    <div className="MeRegistration__heading">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?</div>
-                    <p className="MeRegistration__intro">Swipe right for yes</p>
-                    <p className="MeRegistration__intro">Swipe left for no</p>
-                    <p className="MeRegistration__intro">Swipe down for meh</p>
+                    <Swipeable className="swipeable"
+                    onSwipingRight={this.swipingRight}
+                    onSwipingDown={this.swipingDown}
+                    onSwipingLeft={this.swipingLeft}>
+                        <div className="MeRegistration__heading">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?
+                        </div>
+                        <p className="MeRegistration__intro">Swipe right for yes</p>
+                        <p className="MeRegistration__intro">Swipe left for no</p>
+                        <p className="MeRegistration__intro">Swipe down for meh</p>
+                    </Swipeable>
                 </Container>
             </Container>
 
         return content;
+    },
+
+    swipingRight: function (e) {
+        console.log(e);
+    },
+
+    swipingDown: function (e) {
+        console.log(e);
+    },
+
+    swipingLeft: function (e) {
+        console.log(e);
     }
 });
 
