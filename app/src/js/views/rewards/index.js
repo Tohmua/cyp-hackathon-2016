df44f916b8f0994a784552554ecd4aa8e55eb431
Liam Crewe <liam.crewe@mayden.co.uk>
2016-04-24 11:56:34 +0100

    WIP for rewards

--- app/src/js/views/rewards/index.js
+++ app/src/js/views/rewards/index.js
@@ -1,55 +1,90 @@
-import React from 'react'
-import Container from 'react-container'
-import { GridList, IconButton, StarBorder, GridTile } from 'material-ui'
-import Dialog from 'material-ui/lib/dialog'
+import React from 'react';
+import GridList from 'material-ui/lib/grid-list/grid-list';
+import GridTile from 'material-ui/lib/grid-list/grid-tile';
+import StarBorder from 'material-ui/lib/svg-icons/toggle/star-border';
+import IconButton from 'material-ui/lib/icon-button';
+
 import navigation from '../../lib/navigation'
 
-const rewards = [
-  {
-    name: 'Test name',
-    complete: false
-  },
-  {
-    name: 'Test name 2',
-    complete: true
-  },
-  {
-    name: 'Test name 3',
-    complete: false
-  },
-  {
-    name: 'Test name 4',
-    complete: false
-  },
-  {
-    name: 'Test name 4',
-    complete: true
-  },
-  {
-    name: 'Test name 4',
-    complete: true
-  },
-  {
-    name: 'Test name 4',
-    complete: false
-  },
-  {
-    name: 'Test name 4',
-    complete: true
-  },
-]
+const styles = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    gridList: {
+        overflowY: 'auto',
+    },
+    tile: {
+        padding: '20px'
+    }
+};
+
+const tilesData = [
+    {
+        img: '../../../img/icons/calendar.png',
+        title: '5 day streak',
+    },
+    {
+        img: '../../../img/icons/cogwheel.png',
+        title: '10 day streak',
+    },
+    {
+        img: '../../../img/icons/medal.png',
+        title: '15 day streak',
+    },
+    {
+        img: '../../../img/icons/megaphone.png',
+        title: '20 day streak',
+    },
+    {
+        img: '../../../img/icons/multimedia.png',
+        title: 'Completed a ...',
+    },
+    {
+        img: '../../../img/icons/orientation.png',
+        title: 'temp',
+    },
+    {
+        img: '../../../img/icons/people.png',
+        title: 'temp',
+    },
+    {
+        img: '../../../img/icons/people-1.png',
+        title: 'temp',
+    },
+    {
+        img: '../../../img/icons/ring.png',
+        title: 'temp',
+    },
+    {
+        img: '../../../img/icons/signature.png',
+        title: 'temp',
+    },
+];
 
 const Rewards = React.createClass({
     statics: navigation('Rewards'),
     render () {
-        return (
-            <Container fill className="light-background" scrollable>
-              {rewards.map(reward => 
-                <div className={'trophy ' + (reward.complete ? 'trophy-complete' : 'trophy-incomplete')}>{reward.name}</div>
-              )}
-            </Container>
-        )
+        return <div style={styles.root}>
+            <GridList cols="4" rows="12"
+                      cellHeight={100}
+                      style={styles.gridList}
+            >
+                {tilesData.map(tile => (
+                    <GridTile
+                        key={tile.img}
+                        title={tile.title}
+                        style={styles.tile}
+                    >
+                        <img src={tile.img} onClick={function () {
+                        alert(tile.title)
+                    }}/>
+                    </GridTile>
+                ))}
+            </GridList>
+        </div>
     }
-})
+});
 
-export default Rewards
+export default Rewards;
