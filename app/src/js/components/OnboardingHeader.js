4a625661c40af293ddbc3f96c3cc7b09b93c1317
Tom <tomjharding@live.co.uk>
2016-04-24 12:47:10 +0100

    Logo

--- app/src/js/components/OnboardingHeader.js
+++ app/src/js/components/OnboardingHeader.js
@@ -8,7 +8,7 @@ module.exports = React.createClass({
 	render () {
 		return (
 			<Container align="center" justify="center" direction="column" className="onboarding-header">
-				<img src="./img/logo-mark.svg" className="onboarding-logo" />
+				<img src="./img/logo-mark.png" className="onboarding-logo" />
 				<div className="onboarding-heading onboarding-heading-1">Fancy App Title</div>
 				<div className="onboarding-heading onboarding-heading-2">Fancy App Subtitle</div>
 			</Container>
