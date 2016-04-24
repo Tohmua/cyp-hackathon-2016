8932938d6a83af38b905bbad79cb13fac1bb7caa
Matt Lloyd <matthew.lloyd@mayden.co.uk>
2016-04-24 12:38:22 +0100

    Settings page

--- app/src/js/views/settings/index.js
+++ app/src/js/views/settings/index.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import navigation from '../../lib/navigation'
 import { Container } from '../../touchstone'
-import { Checkbox } from 'material-ui'
+import { Toggle, Checkbox, TextField } from 'material-ui'
+import Divider from 'material-ui/lib/divider';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import SelectField from 'material-ui/lib/select-field';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+
+const switches = [
+    ['Allow notifications'],
+    ['Ask for cancellation reasons', 'When I cancel events']
+].map(([primaryText, secondaryText]) => ({ primaryText, secondaryText }))
+
 
 const Settings = React.createClass({
     statics: navigation('Settings', true),
     render: () => (
         <Container fill className="light-background" scrollable justify="top" direction="column">
-              <h1>Settings</h1>
+            <List>
+                <ListItem><TextField floatingLabelText="Name" /></ListItem>
+                <ListItem><TextField floatingLabelText="Code" type="password"/></ListItem>
+            </List>
+            <Divider />
+            <List>
+                { switches.map((item, i) => 
+                    <ListItem key={ i } { ... item } disabled={ false } rightToggle={<Toggle />} />
+                )}
+            </List>
+            <Divider />
+            <List>
+                { switches.map((item, i) => 
+                    <ListItem key={ i } { ... item } disabled={ false } leftCheckbox={<Checkbox />} />
+                )}
+            </List>
         </Container>
     )
 })
